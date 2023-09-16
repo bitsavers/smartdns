e3c447233e81d25fbd15eb4b1badc851a6e39222
Ahmed <10769951+holazt@users.noreply.github.com>
2023-09-16 18:17:30 +0800

    Update scripts[skip ci]

--- scripts/build-on-docker.sh
+++ scripts/build-on-docker.sh
@@ -37,33 +37,44 @@ mkdir -p /workdir/openssl/build
 cd /workdir/openssl
 if [ "$ARCH" == "mipsel" ] || [ "$ARCH" == "mips" ] ; then
 	OPENSSL_ARCH="linux-mips32"
+	OPENSSL_ARGS="-DBROKEN_CLANG_ATOMICS"
 elif [ "$ARCH" == "mips64el" ] || [ "$ARCH" == "mips64" ] ; then
 	OPENSSL_ARCH="linux-generic64"
+	OPENSSL_ARGS=""
 elif [ "$ARCH" == "powerpc" ] ; then
 	OPENSSL_ARCH="linux-ppc"
+	OPENSSL_ARGS="-DBROKEN_CLANG_ATOMICS"
 elif [ "$ARCH" == "powerpc64" ] ; then
 	OPENSSL_ARCH="linux-generic64"
+	OPENSSL_ARGS=""
 elif [ "$ARCH" == "powerpc64le" ] ; then
 	OPENSSL_ARCH="linux-ppc64le"
+	OPENSSL_ARGS=""
 elif [ "$ARCH" == "arm" ] ; then
 	OPENSSL_ARCH="linux-armv4"
+	OPENSSL_ARGS="-DBROKEN_CLANG_ATOMICS"
 elif [ "$ARCH" == "arm64" ] ; then
 	OPENSSL_ARCH="linux-aarch64"
+	OPENSSL_ARGS=""
 elif [ "$ARCH" == "riscv64" ] ; then
 	OPENSSL_ARCH="linux64-riscv64"
+	OPENSSL_ARGS=""
 elif [ "$ARCH" == "x86" ] ; then
 	OPENSSL_ARCH="linux-x86"
+	OPENSSL_ARGS=""
 elif [ "$ARCH" == "x86_64" ] ; then
 	OPENSSL_ARCH="linux-x86_64"
+	OPENSSL_ARGS=""
 else
 	OPENSSL_ARCH="linux-$ARCH"
+	OPENSSL_ARGS=""
 fi
-perl ./Configure ${OPENSSL_ARCH} no-tests --prefix=/workdir/openssl/build 
+perl ./Configure ${OPENSSL_ARCH} no-tests ${OPENSSL_ARGS} --prefix=/workdir/openssl/build 
 make all -j$(nproc)
 
 cd /workdir
 export CFLAGS="-I /workdir/openssl/build/include -I /workdir/openssl/include"
-export LDFLAGS="-L /workdir/openssl/build/lib"
+export LDFLAGS="-s -Wl,--build-id=sha1 -L /workdir/openssl/build/lib"
 sh ./package/build-pkg.sh --platform debian --arch all --filearch $ARCH-debian --static --outputdir /workdir/build
 sh ./package/build-pkg.sh --platform linux --arch all --filearch $ARCH-linux --static --outputdir /workdir/build
 sh ./package/build-pkg.sh --platform openwrt --arch all --filearch $ARCH-openwrt --static --outputdir /workdir/build
